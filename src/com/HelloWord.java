d2560b33a1b662d77cb5423478cf1e246c645ccd
Zhang Jingliang <zhangjingliang@vip.qq.com>
2016-11-18 23:17:04 +0800

    Initial commit

---
+++ src/com/HelloWord.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by zhang on 2016/11/17.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
